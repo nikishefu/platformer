20a6bbd0135c3bbe18632dda8e68cb5a43ef83b1
lama <tyapkin_24@mail.ru>
2019-12-02 11:37:48 +0300

    Lst  with cell parameters
    Created lst with cell parameters (x, y, x1, y1, id)
    Property not specified

--- render.py
+++ render.py
@@ -5,21 +5,40 @@ WINDOW_SIZE = width, height = 900, 900   # Размер окна (пиксели
 FIELD_SIZE = horizontal, vertical = 10, 5   # Размеры поля (ячейки)
 CELL_SIZE = 50   # Размер одной квадратной клетки (пиксели)
 
+#   Создание списка хранящего в себе клетки (координаты, свойства, id)
+lst = [[[0, 0, 0, 0, ['properties'], 1] for i in range(horizontal)] for j in range(vertical)]
 
-def draw_cells(cell_size, color, field_size):
-    x = y = 0
-    color = pygame.Color(color)
-    for i in range(field_size[1]):
-        for j in range(field_size[0]):
-            pygame.draw.rect(screen, color, (x, y, x + cell_size, y + cell_size), 1)
-            x += cell_size
-        y += cell_size
-    pygame.display.flip()
+#   Создание изначальных характеристик клеток
+x_cell_one = y_cell_one = 0   # Координаты начала клетки
+x_cell_two = y_cell_two = x_cell_one + CELL_SIZE   # Координаты конца клетки
+id = 1   # id каждой клетки
+
+#   Назначении каждой клетки ее координат и id
+for j in range(vertical):
+    for i in range(horizontal):
+
+        #   Присваивание координат клетки
+        lst[j][i][0], lst[j][i][1], lst[j][i][2], lst[j][i][3] = x_cell_one, y_cell_one, x_cell_two, y_cell_two
+        lst[j][i][5] = id
+
+        # Изменение на следующие координаты по X
+        x_cell_one += CELL_SIZE
+        x_cell_two += CELL_SIZE
+
+        id += 1   # Изменение id
+
+    #   Изменение на следующие координаты по Y
+    y_cell_one += CELL_SIZE
+    y_cell_two += CELL_SIZE
+
+    #   Обнуление координат по X
+    x_cell_one = 0
+    x_cell_two = x_cell_one + CELL_SIZE
 
 
+print(lst)
 pygame.init()
 screen = pygame.display.set_mode(WINDOW_SIZE)
-draw_cells(CELL_SIZE, 'white', FIELD_SIZE)
 while pygame.event.wait().type != pygame.QUIT:
     pass
 pygame.quit()
